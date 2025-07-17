fefa0ea4fdda9245371a103a7c2136605cb0625e
toralvyas <toral.vyas@gmail.com>
2025-07-17 16:00:52 +0530

    Update Employee.java

--- _01_02b/Employee.java
+++ _01_02b/Employee.java
@@ -5,26 +5,33 @@ public class Employee {
   public static void main(String[] args) {
 
     // Create a variable called age of type int and assign it the value 29.
-
+int age=29;
     // Print the age variable to the console.
-
+System.out.println(age);
     // Create a variable called isAManager of type boolean and assign it the value
     // true.
+    boolean isAManager =true;
 
     // Print the isAManager variable to the console.
+    System.out.println(isAManager);
 
     // Create a variable called yearsOfService of type double and assign it the
     // value 2.5.
+    double yearsOfService= 2.5;
 
     // Print the yearsOfService variable to the console.
 
     // Create a variable called baseSalary of type int and assign it the value 3000.
-
+int baseSalary=3000; 
     // Create a variable called overtimePayment of type int and assign it the value
     // 40.
 
+    int overtimePayment=400; 
+
     // Create a variable called totalPayment of type int and assign it to the value
     // of baseSalary added to overtimePayment.
+
+    int totalPayment = baseSalary+overtimePayment;
 
     // Print the totalPayment variable to the console.
 
